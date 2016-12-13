77438712ea10cd0d1b58ff727b46c60e37a71659
rohan <rtabish@illinois.edu>
2016-12-13 17:16:57 -0600

    Updated Run Script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 cd daggen-master-rtmod   # Change directory to the daggen task
 
-./daggen -n 10 --tasks --cpus=2 
+./daggen -n 10 --minT=10 --maxT=100 --minC=1 --maxC=2 --sendBudget=10 --recvBudget=10 --cpus=2 --tasks 
 
 cd ../
 
